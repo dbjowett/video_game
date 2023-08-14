43006aa5c34c33a26e9f9a24884eda5210074e0f
dbjowett <dbjowett@gmail.com>
2023-08-14 19:33:24 +0900

    Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,10 @@ import Head from "next/head";
 import GameList from "~/components/GameList";
 
 export default function Home() {
+  const Search = () => {
+    return <input type="text" />;
+  };
+
   return (
     <>
       <Head>
@@ -13,6 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center  ">
+        <Search />
         <GameList small type="popular" />
         <GameList small type="toprated" />
         <GameList small type="upcoming" />
