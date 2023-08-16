86b730a0316e57375875fb95543db22c71174931
dbjowett <dbjowett@gmail.com>
2023-08-17 07:18:47 +0900

    Cache access token logic

--- src/pages/api/utils/twitch_auth.ts
+++ src/pages/api/utils/twitch_auth.ts
@@ -1,48 +1,29 @@
 import axios from "axios";
 import NodeCache from "node-cache";
-import { scheduleJob } from "node-schedule";
 import { type TwitchParams, type TwitchResponse } from "./types";
 
 const cache = new NodeCache();
 const accessTokenUri = "https://id.twitch.tv/oauth2/token";
-let tokenExpirationTime: number;
 
 export const getTwitchAuth = async (
   twitchParams: TwitchParams
 ): Promise<string> => {
-  const queryString = Object.keys(twitchParams)
+  const queryString = Object.entries(twitchParams)
     .map(
-      (key) =>
-        `${encodeURIComponent(key)}=${encodeURIComponent(
-          twitchParams[key as keyof typeof twitchParams]
-        )}`
+      ([key, value]) =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`
     )
     .join("&");
 
   const url = `${accessTokenUri}?${queryString}`;
+  const cachedToken = cache.get("access_token");
 
-  if (tokenExpirationTime && tokenExpirationTime > Date.now()) {
-    const currentToken = cache.get("access_token") as string | null;
-    if (currentToken !== null) {
-      return currentToken;
-    }
-  }
-
-  try {
+  if (!!cachedToken) {
+    return cache.get("access_token")!;
+  } else {
     const { data }: { data: TwitchResponse } = await axios.post(url);
     const expiresIn = data.expires_in;
-    tokenExpirationTime = Date.now() + expiresIn * 1000;
     cache.set("access_token", data.access_token, expiresIn);
-    scheduleJob(new Date(tokenExpirationTime), async () => {
-      try {
-        await getTwitchAuth(twitchParams);
-      } catch (error) {
-        console.log(`Couldn't refresh token: `, error);
-      }
-    });
     return data.access_token;
-  } catch (error) {
-    console.log(`Couldn't get authorization: `, error);
-    throw new Error("Getting Access Token Failed");
   }
 };
