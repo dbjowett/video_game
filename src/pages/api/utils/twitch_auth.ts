8cb07b6d15777653f6de0dd2c5b98dc35a4013b5
dbjowett <dbjowett@gmail.com>
2023-08-17 07:31:51 +0900

    Change logic so loop doesn't run while getting auth if not needed

--- src/pages/api/utils/twitch_auth.ts
+++ src/pages/api/utils/twitch_auth.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import NodeCache from "node-cache";
+import { z } from "zod";
 import { type TwitchParams, type TwitchResponse } from "./types";
 
 const cache = new NodeCache();
@@ -8,22 +9,23 @@ const accessTokenUri = "https://id.twitch.tv/oauth2/token";
 export const getTwitchAuth = async (
   twitchParams: TwitchParams
 ): Promise<string> => {
-  const queryString = Object.entries(twitchParams)
-    .map(
-      ([key, value]) =>
-        `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`
-    )
-    .join("&");
-
-  const url = `${accessTokenUri}?${queryString}`;
+  const stringSchema = z.string();
   const cachedToken = cache.get("access_token");
-
-  if (!!cachedToken) {
-    return cache.get("access_token")!;
+  if (cachedToken) {
+    return stringSchema.parse(cachedToken);
   } else {
-    const { data }: { data: TwitchResponse } = await axios.post(url);
-    const expiresIn = data.expires_in;
-    cache.set("access_token", data.access_token, expiresIn);
-    return data.access_token;
+    const url = `${accessTokenUri}?${Object.entries(twitchParams)
+      .map(
+        ([key, value]) =>
+          `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`
+      )
+      .join("&")}`;
+
+    const {
+      data: { expires_in, access_token },
+    }: { data: TwitchResponse } = await axios.post(url);
+    const expiresIn = expires_in;
+    cache.set("access_token", access_token, expiresIn);
+    return access_token;
   }
 };
