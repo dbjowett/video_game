92ecabf9d8aa967fea12ef98c05882d9b16fcc8a
dbjowett <dbjowett@gmail.com>
2023-08-18 09:44:51 +0900

    Fix types

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
+import { type ReactNode } from "react";
 import { NavBar } from "./Navbar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="bg-slate-100 pb-10">
       <NavBar />
