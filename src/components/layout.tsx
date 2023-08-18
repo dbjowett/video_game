09000b36b803106c6d236f15a08aa40d20f6d408
dbjowett <dbjowett@gmail.com>
2023-08-18 09:48:37 +0900

    Layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,11 @@
 import { type ReactNode } from "react";
 import { NavBar } from "./Navbar";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="bg-slate-100 pb-10">
       <NavBar />
       {children}
     </div>
   );
-}
+};
