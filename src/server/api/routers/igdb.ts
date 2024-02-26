5e73ea778fa523e191b33d451013d3547870b7e8
dbjowett <dbjowett@gmail.com>
2024-02-26 09:37:02 +0900

    Add query contructor

--- src/server/api/routers/igdb.ts
+++ src/server/api/routers/igdb.ts
@@ -2,9 +2,12 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import igdb from "~/pages/api/utils/igdb";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+import { constructQuery } from "~/utils/query_constructor";
 import { GameValidator, type Game } from "../schemas/games";
 
 export const igdbRouter = createTRPCRouter({
+  // ** Get game by id. Returns a single game **
   getGameById: publicProcedure
     .input(
       z.object({
@@ -12,15 +15,30 @@ export const igdbRouter = createTRPCRouter({
       })
     )
     .query(async ({ input }) => {
+      const query_data = constructQuery({
+        fields: [
+          "name",
+          "rating",
+          "rating_count",
+          "release_dates.*",
+          "summary",
+          "similar_games",
+          "screenshots.image_id",
+          "cover.*",
+          "rating",
+          "genres.name",
+          "platforms.*",
+        ],
+        where: `id=${input.id}`,
+        limit: 1,
+      });
+
       const reqOptions = {
         method: "POST",
-        data: `
-        fields name, rating, rating_count, release_dates.*, summary, similar_games, screenshots.image_id, cover.*, rating, genres.name, platforms.*;
-        where id=${input.id};
-        limit 1;
-      `,
+        data: query_data,
         url: "/games/",
       };
+
       const { data }: { data: Game[] } = await igdb(reqOptions);
 
       if (!data) {
@@ -38,4 +56,6 @@ export const igdbRouter = createTRPCRouter({
       }
       return valid.data;
     }),
+
+  // ** Get Upcoming Games. Returns an array of games
 });
---
+++ src/utils/query_constructor.ts
@@ -0,0 +1,108 @@
+type GameField =
+  | "age_ratings"
+  | "aggregated_rating"
+  | "aggregated_rating_count"
+  | "alternative_names"
+  | "artworks"
+  | "bundles"
+  | "category"
+  | "checksum"
+  | "collection"
+  | "collections"
+  | "cover"
+  | "created_at"
+  | "dlcs"
+  | "expanded_games"
+  | "expansions"
+  | "external_games"
+  | "first_release_date"
+  | "forks"
+  | "franchise"
+  | "franchises"
+  | "game_engines"
+  | "game_modes"
+  | "genres"
+  | "hypes"
+  | "involved_companies"
+  | "keywords"
+  | "language_supports"
+  | "multiplayer_modes"
+  | "name"
+  | "parent_game"
+  | "platforms"
+  | "player_perspectives"
+  | "ports"
+  | "rating"
+  | "rating_count"
+  | "release_dates"
+  | "remakes"
+  | "remasters"
+  | "screenshots"
+  | "similar_games"
+  | "slug"
+  | "standalone_expansions"
+  | "status"
+  | "storyline"
+  | "summary"
+  | "tags"
+  | "themes"
+  | "total_rating"
+  | "total_rating_count"
+  | "updated_at"
+  | "url"
+  | "version_parent"
+  | "version_title"
+  | "videos"
+  | "websites";
+
+type GameFieldWithSubfields = `${GameField}.*`;
+type GameFieldWithSpecificSubfields = `${GameField}.${string}`;
+type CompleteGameField =
+  | GameField
+  | GameFieldWithSubfields
+  | GameFieldWithSpecificSubfields;
+
+export interface IGDBQueryOptions {
+  fields: CompleteGameField[]; // List of fields to fetch
+  where?: string; // Advanced filtering using the `where` syntax
+  limit?: number; // How many records to return
+  search?: string; // A search query
+}
+
+// ** @link https://api-docs.igdb.com/#game
+
+/**
+ * Construct a query string for the IGDB API.
+ *
+ * @param options Configuration options for the query.
+ * @returns The constructed query string.
+ */
+export const constructQuery = (options: IGDBQueryOptions): string => {
+  const queryParts = [] as string[];
+
+  // Fields part
+  if (options.fields && options.fields.length > 0) {
+    const fieldsPart = `fields ${options.fields.join(", ")};`;
+    queryParts.push(fieldsPart);
+  }
+
+  // Where part
+  if (options.where) {
+    const wherePart = `where ${options.where};`;
+    queryParts.push(wherePart);
+  }
+
+  // Limit part
+  if (options.limit) {
+    const limitPart = `limit ${options.limit};`;
+    queryParts.push(limitPart);
+  }
+
+  // Search part, if applicable
+  if (options.search) {
+    const searchPart = `search "${options.search}";`;
+    queryParts.push(searchPart);
+  }
+
+  return queryParts.join(" ");
+};
